7dbda2b8f3b8c6c5fab2e9a0e7b4acd522dcc86d
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-03-12 13:13:23 -0400

    TestRunner have updates

--- src/test/java/ca/amazon/runners/TestRunner.java
+++ src/test/java/ca/amazon/runners/TestRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+//Updates in TestRunner
 @CucumberOptions(
 		features="C:\\Users\\shiva\\eclipse-workspace\\amazon\\src\\test\\resources\\features",
 		glue= {"ca.amazon.stepdefinitions","ca.amazon.apphooks"},
